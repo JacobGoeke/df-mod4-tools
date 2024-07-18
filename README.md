93575605f5011da7358876a329ab289e604eaf0f
Jacob Goeke <s557365@nwmissouri.edu>
2024-07-18 03:29:42 -0500

    committing changes

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # df-mod4-tools
+
+## Known Files Hash Databases
+
+### Hash databases can be very helpful for identifying known files so that you do not have them in your way during your investigation. Below is an important hash database into the forensic investigation tool Autopsy.
+
+![Screenshot](images\hashDB.png)
+
+## Known Bad Files in Hash Databases
+
+### Known bad files in hash databases are important for investigators to pick up on evidence when performing an investigation. An attached screenshot is showing an example of a database created in Autopsy for known evidence files.
+
+![Screenshot](images\knownbadhash.png)
+
+## Using a Hex Editor to View File Remnants
+
+### We can examine files in a hex editor that have been deleted as long as the place in their drive has not overwritten the space that the file used to reside in. The screenshot below shows a hash computed from the first sector of a docx file.
+
+![Screenshot](images\1stSectorhash.PNG)
+
+## Bit Shifting
+
+### Bit shifting can be used to encrypt files by shifting the bits a desired amount. This can keep your file secure but can also result in the loss of data. In digital forensics, a suspect may be using bit shifting to mask evidence and the investigator must be aware of ways to recover data that has been bit shifted. Attached is a screenshot of a bit shifted message. We can see the new message is nothing like the old one.
+
+![Screenshot](images\encode.PNG)
+
+## Rainbow Tables
+
+### Rainbow tables can be very useful in cracking passwords. They are essentially a respository of all password hashes, and the investigator must compare a suspected password hash to the repository and will be able to derive the password.
---
+++ rainbowtable/Findhash.ps1
@@ -0,0 +1,72 @@
+# This file will find a hash in our recently created rainbow table
+$erikHash = "9e89895d350e5fdac013006b2acb067f8516149cdf7e952b021ff0326718ab70"
+$loganHash = "af95a5393589cace29a63eead5328f1647a8bc62b5ef18b023c574484a877ced"
+$codyHash = "30fc150e7c8b42dd86b1bb6e67ee256be8230969bb37cc111749e87af383ae82"
+
+$rainbowTable = Get-Content -Path rainbowtable.txt
+
+$foundCOde = $null
+
+
+# Finding Erik's code
+foreach ($entry in $rainbowTable) {
+    # Split the entry into code and hash parts
+    $parts = $entry -split ","
+    $code = $parts[0]
+    $hash = $parts[1]
+
+    # Check if the hash matches the target hash
+    if ($hash -eq $erikHash) {
+        $foundCode = $code
+        break
+    }
+}
+
+# Output the result
+if ($foundCode) {
+    Write-Output "Code corresponding to Erik's hash is: $foundCode"
+} else {
+    Write-Output "Hash not found in the rainbow table."
+}
+
+# Finding Logan's code
+foreach ($entry in $rainbowTable) {
+    # Split the entry into code and hash parts
+    $parts = $entry -split ","
+    $code = $parts[0]
+    $hash = $parts[1]
+
+    # Check if the hash matches the target hash
+    if ($hash -eq $loganHash) {
+        $foundCode = $code
+        break
+    }
+}
+
+# Output the result
+if ($foundCode) {
+    Write-Output "Code corresponding to Logan's hash is: $foundCode"
+} else {
+    Write-Output "Hash not found in the rainbow table."
+}
+
+# Finding Cody's code
+foreach ($entry in $rainbowTable) {
+    # Split the entry into code and hash parts
+    $parts = $entry -split ","
+    $code = $parts[0]
+    $hash = $parts[1]
+
+    # Check if the hash matches the target hash
+    if ($hash -eq $codyHash) {
+        $foundCode = $code
+        break
+    }
+}
+
+# Output the result
+if ($foundCode) {
+    Write-Output "Code corresponding to Cody's hash is: $foundCode"
+} else {
+    Write-Output "Hash not found in the rainbow table."
+}
