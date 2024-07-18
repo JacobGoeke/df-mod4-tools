bd03ba213de7aa268d2f111477e3e2319d0b9d03
Jacob Goeke <s557365@nwmissouri.edu>
2024-07-18 03:31:15 -0500

    nothing here

--- Findhash.ps1
+++ Findhash.ps1
@@ -3,7 +3,7 @@ $erikHash = "9e89895d350e5fdac013006b2acb067f8516149cdf7e952b021ff0326718ab70"
 $loganHash = "af95a5393589cace29a63eead5328f1647a8bc62b5ef18b023c574484a877ced"
 $codyHash = "30fc150e7c8b42dd86b1bb6e67ee256be8230969bb37cc111749e87af383ae82"
 
-$rainbowTable = Get-Content -Path rainbowtable\rainbowtable.txt
+$rainbowTable = Get-Content -Path rainbowtable.txt
 
 $foundCOde = $null
 
